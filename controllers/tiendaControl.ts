f158b4847293a048181403185616957319bea9ff
juleneetac <univideos8@gmail.com>
2020-12-31 12:25:28 +0100

    cuando no hay dinero suficiente en cartera da un mensaje

--- controllers/tiendaControl.ts
+++ controllers/tiendaControl.ts
@@ -33,28 +33,31 @@ async function pubKeybanco() {   //el server(banco) me pasa su pubKey para verif
 async function postpayverify(req, res){
     try {
 
-        verificame = req.body.verificame //esto es la moneda(array de 1€)
-        
-        cantidad = req.body.cantidad  //esto es la cantidad de dinero(que sera igual al length del array)
-        let y = 0;
-        while (y < verificame.length){
-            verifiedm[y] = bc.bigintToText(pubKeyBanco.verify(bc.hexToBigint(verificame[y])))  //esto ,lo hara la tienda que es quien verifique la moneda
-            y++;
+      verificame = req.body.verificame //esto es la moneda(array de 1€)
+      cantidad = req.body.cantidad  //esto es la cantidad de dinero(que sera igual al length del array)
+      console.log(cantidad)
+      let y = 0;
+
+      while (y < verificame.length){
+              verifiedm[y] = bc.bigintToText(pubKeyBanco.verify(bc.hexToBigint(verificame[y])))  //esto ,lo hara la tienda que es quien verifique la moneda
+              y++;
       }
-       let resultado = await postbancverify(verifiedm,cantidad)
-       console.log(resultado)
+      let resultado = await postbancverify(verifiedm,cantidad)
+      console.log(resultado)
       if(resultado == "zi"){
-        res.status(200).send({msg: "la moneda es valida"})
+          res.status(200).send({msg: "la moneda es valida"})
       }
       else {
-        res.status(501).send({msg: "estafador"})
-      }
+          res.status(501).send({msg: "estafador"})
+        }
       }
       catch(err) {
         console.log(err)
         res.status(500).send ({ message: err})
       }
 }
+
+
 async function postbancverify(verificame,cantidad):Promise<string>{
   let resultado
   await axios
